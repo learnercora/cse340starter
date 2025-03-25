cf49c67bec33706ad57746d248a06999b81d8456
coralin <zxc188950@yahoo.com.tw>
2025-03-25 13:10:40 +0800

    w4-2

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,13 +3,18 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
 
 // login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 //register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
-//
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+//建立會員 view
+router.post('/register', 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
 
 
 module.exports = router;
